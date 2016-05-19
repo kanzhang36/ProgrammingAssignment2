dff71da4759a4e90a5d0e999bc228937e1b4092b
kanzhang36 <kanzhang36@gmail.com>
2016-05-18 23:52:36 -0400

    update

--- cachematrix.R
+++ cachematrix.R
@@ -11,6 +11,7 @@ makeCacheMatrix <- function(x = matrix()) {
                 x <<- y
                 m <<- NULL
         }
+        
         get <- function() x
         setinverse <- function(inverse) m <<- inverse
         getinverse <- function() m
